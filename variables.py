241e24ab71218c06923464f2c803587b407a9955
ViolDio <dolinina170696@gmail.com>
2024-11-07 16:36:30 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,7 @@
+quantity_completed_homework = 12
+quantity_spent_hours = 1.5
+course_name = 'Python'
+print ( 1.5 / 12 )
+time_per_task = 0.125
+
+print (course_name,'всего задач:',quantity_completed_homework,'затрачено часов:',quantity_spent_hours,'среднее время выполнения', time_per_task)
